6677fd422de3e3f846e34c2b655fac89e27823e3
GitKyleHere1 <58244563+GitKyleHere1@users.noreply.github.com>
2022-05-23 20:32:01 -0500

    Add files via upload

---
+++ Chapter 02-04 Madlibs.py
@@ -0,0 +1,26 @@
+# Print some lyrics to The Beatles, "Hey " + person + "".
+
+person = input("Please enter a name of a person.")
+greeting = input("Please enter a greeting.")
+sense = input("Please enter a sense.")
+place = input("please enter a place (possessive).")
+thing = input("please enter a thing.")
+person_type = input("please enter a type of person. (fool, genius, coward, etc).")
+body_part = input("please enter a body part from the human body.")
+emotion = input("please enter an emotion.")
+
+print("\n\n")
+print(greeting + " " + person + ", don't make it bad.")
+print("Take a " + emotion + " " + thing + " and make it better.")
+print("Remember to let her into your " + body_part + ",")
+print("Then you can start to make it better. \n")
+
+print(greeting + " " + person + ", don't be " + emotion + ".")
+print("You were made to go out and get " + thing + ".")
+print("The minute you let her under your " + thing + ",")
+print("Then you begin to make it better. \n")
+
+print("And anytime you " + sense + " the " + thing + ", hey " + person + ", refrain,")
+print("Don't carry the " + thing + " upon your " + body_part + ".")
+print("For well you know that it's a " + person_type + " who plays it cool")
+print("By making his " + place + " a little colder.")
