5f48f554bdc2ee1b5d7b15b055044cb41761a34e
GitKyleHere1 <58244563+GitKyleHere1@users.noreply.github.com>
2022-06-14 10:40:29 -0500

    Update Menu.py

--- chapter projects/chapter 5/Menu.py
+++ chapter projects/chapter 5/Menu.py
@@ -6,11 +6,11 @@ def main():
     # Initialize dictionary
 
     menu = {
-        1: ["Black Desert", "An online MMORPG game that takes a lot of time investment to reach end game."],
-        2: ["Apex", "A battle royale game with futuristic elements."],
-        3: ["Starfield", "A RPG game from Bethesda coming in 2023."],
-        4: ["Cyberpunk 2077", "A futuristic RPG game that had high expectations but lots of bugs at release."],
-        5: ["Escape From Tarkov", "A survival looter shooter game with that keeps your adrenaline rushing."]
+        1: ["Black Desert", black_desert],
+        2: ["Apex", apex],
+        3: ["Starfield", starfield],
+        4: ["Cyberpunk 2077", cyberpunk],
+        5: ["Escape From Tarkov", escape_from_tarkov]
     }
 
     # call the print_list function to display the menu
@@ -26,18 +26,17 @@ def main():
         selection = input("please select a number from the list to learn more about that game. ")
 
     selection = int(selection)
-    print("\n" + menu[selection][0] + "\n" + menu[selection][1])
+    print("\n" + menu[selection][0] + "\n" + menu[selection][1]())
 
-
+# Function to print the menu
 def print_list(menu):
     print("Game list")
     for key, value in menu.items():
         print(str(key) + ":", value[0])
     print("\n")
 
-
+# Function to check if selection is valid
 def invalid_selection(menu, selection):
-
     if selection.isdigit():
         selection = int(selection)
 
@@ -45,6 +44,27 @@ def invalid_selection(menu, selection):
         return False
     else:
         return True
+
+# The following are functions that are used to return the description of each game to main to be displayed to the user.
+
+def black_desert():
+    return "An online MMORPG game that takes a lot of time investment to reach end game."
+    
+    
+def apex():
+    return "A battle royale game with futuristic elements."
+    
+    
+def starfield():
+     return "A RPG game from Bethesda coming in 2023."
+    
+    
+def cyberpunk():
+    return "A futuristic RPG game that had high expectations but lots of bugs at release."
+    
+    
+def escape_from_tarkov():
+    return "A survival looter shooter game with that keeps your adrenaline rushing."
 
 
 main()
